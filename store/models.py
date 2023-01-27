b7c2df0ff3b5375ce0d06a041fa9a2bddbbd160a
Mahsa Kamalianaraki <mahsa.kamalianaraki@gmail.com>
2023-01-26 23:30:02 -0500

    Change first_name to given_name

--- store/models.py
+++ store/models.py
@@ -31,7 +31,7 @@ class Customer(models.Model):
         (MEMBERSHIP_BRONZE, 'Bronze'),
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold')]
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
